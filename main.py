e3b08a488f674cbd3aae2f7b953ccf6f12164cb3
jamkinder <aekshov@yandex.ru>
2023-12-26 11:00:44 +0300

    started make upgrade system

--- main.py
+++ main.py
@@ -90,7 +90,7 @@ while running:
         selected_turret.selected = True  # показываем диапазон
     # отрисовываем башни
     for turrets in turret_group:
-        turrets.draw(screen,ev)
+        turrets.draw(screen)
 
     enemy_group.update()
     turret_group.update(enemy_group, screen)
