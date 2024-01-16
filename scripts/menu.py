616ac5f38316ea97f21426e8991f9ec4e2aaf741
marsin <lampetow555@gmail.com>
2024-01-16 21:20:32 +0300

    The menu has been edited

--- scripts/menu.py
+++ scripts/menu.py
@@ -26,28 +26,39 @@ def menu(surface):
             if event.type == pygame.MOUSEBUTTONUP:
                 x, y = event.pos
                 # если кликнули по кнопке начать, начинаем игру
-                if 180 <= x <= 180 + 150 and 230 <= y <= 305:
+                if (const.SCREEN_WIDTH // 2 - 70 <= x <= const.SCREEN_WIDTH // 2 - 70 + 150
+                        and const.SCREEN_HEIGHT // 2 - 20 <= y <= const.SCREEN_HEIGHT // 2 + 55):
                     visual.music_click.play()
                     return True
                 # если кликнули по кнопке выйти, заканчиваем игру
-                elif 180 <= x <= 180 + 150 and 230 * 1.5 <= y <= 305 * 1.5:
+                elif (const.SCREEN_WIDTH // 2 - 70 <= x <= const.SCREEN_WIDTH // 2 - 70 + 150
+                      and (const.SCREEN_HEIGHT // 2 - 20) * 1.5 <= y <= (const.SCREEN_HEIGHT // 2 - 20) * 1.5 + 75):
                     return False
         # показываем фон
         surface.blit(fon, (0, 0))
         # показываем кнопки
         for i in range(len(text_button)):
             x, y = pygame.mouse.get_pos()
-            surface.blit(image_button, (180, 230 * (1 + i * 0.5)))
+            surface.blit(image_button, (const.SCREEN_WIDTH // 2 - 70, 230 * (1 + i * 0.5)))
 
             # если навелись на кнопку, то изменяем её размер
-            if 180 <= x <= 180 + 150 and 230 * (1 + i * 0.5) <= y <= 305 * (1 + i * 0.5):
-                surface.blit(active_text_button[i], (215 + 5, 250 * (1 + i * 0.45) + 5))
+            if (const.SCREEN_WIDTH // 2 - 70 <= x <= const.SCREEN_WIDTH // 2 - 70 + 150
+                    and (const.SCREEN_HEIGHT // 2 - 20) * (1 + i * 0.5) <= y <= (
+                            const.SCREEN_HEIGHT // 2 - 20) * (1 + i * 0.5) + 75):
+                surface.blit(active_text_button[i],
+                             (const.SCREEN_WIDTH // 2 - 30, const.SCREEN_WIDTH // 2 * (1 + i * 0.45) + 5))
+
                 if not hooked:
                     visual.music_hooked.play()
                     hooked = True
             else:
-                surface.blit(text_button[i], (215, 250 * (1 + i * 0.45)))
-                if not (180 <= x <= 180 + 150 and 230 <= y <= 305) and not (
-                        180 <= x <= 180 + 150 and 230 * 1.5 <= y <= 305 * 1.5):
+                surface.blit(text_button[i], (const.SCREEN_WIDTH // 2 - 35, const.SCREEN_WIDTH // 2 * (1 + i * 0.45)))
+
+                if (not (const.SCREEN_WIDTH // 2 - 70 <= x <= const.SCREEN_WIDTH // 2 - 70 + 150
+                         and (const.SCREEN_HEIGHT // 2 - 20) <= y <= (const.SCREEN_HEIGHT // 2 - 20) + 75)
+                        and not (
+                                const.SCREEN_WIDTH // 2 - 70 <= x <= const.SCREEN_WIDTH // 2 - 70 + 150 and
+                                (const.SCREEN_HEIGHT // 2 - 20) * 1.5 <= y <= (
+                                        const.SCREEN_HEIGHT // 2 - 20) * 1.5 + 75)):
                     hooked = False
         pygame.display.flip()
